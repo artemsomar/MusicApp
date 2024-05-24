55409adbfdf5aab26ed65db0ee349241d87bdc35
Artem Somar <artemsomar0@gmail.com>
2024-05-24 06:16:26 +0300

    Initial Commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Artem Somar on 23.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
