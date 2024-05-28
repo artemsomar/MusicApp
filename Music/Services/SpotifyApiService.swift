732900073091ee51872e0d7d991dd1fe99bffe06
Artem Somar <artemsomar0@gmail.com>
2024-05-28 15:47:27 +0300

    Working ScrollView, doesn't work API

---
+++ Music/Services/SpotifyApiService.swift
@@ -0,0 +1,8 @@
+//
+//  SpotifyApiService.swift
+//  Music
+//
+//  Created by Artem Somar on 28.05.2024.
+//
+
+import Foundation
---
+++ Music/Utilities/NetworkingManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkingManager.swift
+//  Music
+//
+//  Created by Artem Somar on 28.05.2024.
+//
+
+import Foundation
